23d36ce2b78e51e116e6cbfe5f2342fea2ca06e6
JasonCarvalho-tech <jml123carvalho@gmail.com>
2020-02-27 17:53:21 +0530

    created box component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Box extends React.Component {
+    render() {
+        return (
+            <div
+                className={this.props.boxClass}
+                id={this.props.id}
+                onClick={this.selectBox}
+                />
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Grid extends React.Component {
+    render() {
+        const width = this.props.cols * 14;
+        var rowsArr = []
+
+        var boxClass = "";
+        for (var i = 0; i < this.props.rows; i++) {
+            for (var j = 0; j < this.props.cols; j++) {
+                let boxId = i + "_" +j;
+
+                boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+                rowsArr.push(
+                    <Box
+                        boxClass={boxClass}
+                        key={boxId}
+                        boxId={boxId}
+                        row={i}
+                        col={j}
+                        selectBox={this.props.selectBox}
+                    />
+                );
+            }
+        }
+
+        return(
+            <div className="grid" style={{width: width}}>
+                {{rowsArr}}
+            </div>
+        );
+    }
+}
+
+class Main extends React.Component {
+    constructor() {
+        super();
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
+        selectBox=(this.selectBox)
+
+        this.state = {
+            generation: 0,
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>The Game of Life</h1>
+                <Grid
+                    gridFull={this.state.gridFull}
+                    rows={this.rows}
+                    cols={this.cols}
+                />
+                <h2>Generations: {this.state.generation}</h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
